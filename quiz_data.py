b26dfb93a2f64f2df6caffadf7df1c3e0d291b34
Aryakul14 <160509697+Aryakul14@users.noreply.github.com>
2024-02-19 20:25:03 +0530

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,22 @@
+quiz_data = [
+    {
+        "question" :"Who is Harry Potter's best friend?",
+        "choices" : ["Draco Mlafoy","Neville Longbottom","Ron Weasley","Cedric Diggory"],
+        "answer" : "Ron Weasley"
+    },
+    {
+        "question" : "Which animal is Hermione's patronus?",
+        "choices" : ["Otter", "Dog", "Cat","Elephant"],
+        "answer" : "Otter"
+    },
+    {
+        "question" : "How many schools participated in the Tri-wizard Tournament?",
+        "choices": ["2","4","3","5"],
+        "answer" : "3"
+    },
+    {
+        "question" : "How many horcruxes does Voldemort make?",
+        "choices" : ["4","7","9","3"],
+        "answer" : "7"
+    }
+]
